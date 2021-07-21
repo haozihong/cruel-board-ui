6e386fecc800a5bca4db26bfe36402e916ccb5c8
Zihong Hao <haozihong@hotmail.com>
2021-07-21 17:51:37 -0400

    read style from excel; add contest sorting function

--- src/components/CruelBoardTable.vue
+++ src/components/CruelBoardTable.vue
@@ -3,7 +3,7 @@
     <el-button size="small" v-on:click="loadMore(5)">Load 5 More Contests</el-button>
     <el-button size="small" v-on:click="loadMore(-1)">Load All Contests</el-button>
     <el-table
-        :data="boardData"
+        :data="qunyouData"
         style="width: 100%"
         stripe
         height="calc(100% - 32px)"
@@ -27,18 +27,26 @@
         </template>
       </el-table-column>
       <el-table-column
+          align="center"
           prop="days"
-          label="days"
+          label="Days"
           width="80"
           sortable>
       </el-table-column>
       <el-table-column
+          align="center"
           prop="lcRating"
           label="Rating"
           width="100"
           sortable>
+        <template v-slot="scope">
+          <span :style="`color: #${scope.row.lcRating >= 2100 ? 'A30000' : '0426A4'}`">
+            {{ scope.row.lcRating }}
+          </span>
+        </template>
       </el-table-column>
       <el-table-column
+          align="center"
           prop="cruelScore"
           label="Score"
           width="100"
@@ -47,13 +55,16 @@
           :sort-orders="['ascending', 'descending']">
       </el-table-column>
       <el-table-column
+          align="center"
           v-for="c in this.contestsShown" :key="c.id"
+          :prop="`contest${c.contestIndex}Ranking`"
+          :label="c.contestIndex + ''"
+          sortable
           width="100">
-        <template slot="header">
-          {{ c.contestIndex }}
-        </template>
         <template v-slot="scope">
-          {{ `${scope.row.contestRankings[c.id].ranking} | ${scope.row.contestRankings[c.id].score}` }}
+          <div :style="`background: #${scope.row[`contest${c.contestIndex}RankingClr`]}; color: black`">
+            {{ `${scope.row[`contest${c.contestIndex}Ranking`]} | ${scope.row[`contest${c.contestIndex}Score`]}` }}
+          </div>
         </template>
       </el-table-column>
 
@@ -71,7 +82,7 @@ export default {
       refresh: 0,
       contests: [],
       contestsShown: [],
-      boardData: []
+      qunyouData: []
     }
   },
   methods: {
@@ -85,15 +96,16 @@ export default {
   mounted() {
     this.axios.get("./index.xlsx", {responseType: "arraybuffer"}).then(resp => {
       let data = new Uint8Array(resp.data);
-      let wb = XLSX.read(data, {type: 'array'});
+      let wb = XLSX.read(data, {type: 'array', cellStyles: true});
       let ws = wb.Sheets[wb.SheetNames[0]];
-      // console.log(ws['B10']);
+      // console.log(ws['F79']);
 
+      // find first row of participants data
       let firstEntryRow = 0;
       while (ws[XLSX.utils.encode_cell({c: 0, r: firstEntryRow})] === undefined)
         ++firstEntryRow;
 
-      // read contests
+      // read contests info
       let firstContextCol = 5;
       for (let i = firstContextCol;
            ws[XLSX.utils.encode_cell({r: firstEntryRow-3, c: i})] !== undefined;
@@ -105,7 +117,7 @@ export default {
         });
       }
 
-      // read each person
+      // read each qunyou
       for (let i = firstEntryRow; ws[`A${i+1}`] !== undefined; ++i) {
         let person = {
           cruelRanking: ws[`A${i+1}`].v,
@@ -114,20 +126,28 @@ export default {
           days: ws[`C${i+1}`].v,
           lcRating: ws[`D${i+1}`].v,
           cruelScore: ws[`E${i+1}`].v,
-          contestRankings: []
+          // contestRankings: []
         };
         for (let j=0; j<this.contests.length; ++j) {
-          person.contestRankings.push({
-            ranking: ws[XLSX.utils.encode_cell({r: i, c: 5+j*2})].v,
-            score: ws[XLSX.utils.encode_cell({r: i, c: 6+j*2})].v
-          });
+          let ranking = ws[XLSX.utils.encode_cell({r: i, c: 5+j*2})].v,
+              rankingClr = ws[XLSX.utils.encode_cell({r: i, c: 5+j*2})].s.fgColor?.rgb ?? 'EAEAEA',
+              score = ws[XLSX.utils.encode_cell({r: i, c: 6+j*2})].v;
+          if (ranking < 0) ranking = Infinity;
+          person[`contest${this.contests[j].contestIndex}Ranking`] = ranking;
+          person[`contest${this.contests[j].contestIndex}RankingClr`] = rankingClr;
+          person[`contest${this.contests[j].contestIndex}Score`] = score;
+          // person.contestRankings.push({
+          //   ranking: ranking,
+          //   rankingClr: rankingClr,
+          //   score: score,
+          // });
         }
-        this.boardData.push(person);
-        // console.log(person);
-        // if (i > 45) break;
+        this.qunyouData.push(person);
+        // if (i > 25) break;
       }
+      // console.log(this.qunyouData[0]);
 
-      // this.contestsShown = this.contests;
+      // show 5 contests initially
       this.contestsShown = this.contests.slice(0, 5);
     });
   }
@@ -136,17 +156,6 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
 a {
   color: #42b983;
 }
