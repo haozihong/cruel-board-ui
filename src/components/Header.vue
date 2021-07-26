3c101ae240ec0e3d46e3240e5dd71e1d3bf293aa
Zihong Hao <haozihong@hotmail.com>
2021-07-26 18:37:03 -0400

    implement opening links in new window

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="header">
-    <el-link id="logo-title" href="https://wisdompeak.github.io/lc-score-board/" :underline="false">
+    <el-link id="logo-title" href="https://wisdompeak.github.io/lc-score-board/" target="_blank" :underline="false">
       <div>
         <img src="../assets/YouXiuLogo.png" alt="logo">
         <h1 class="hidden-xs-only">残酷刷题群</h1>
@@ -10,7 +10,7 @@
     <div id="links">
       <el-link :underline="false" @click="joiningInfoVisible = true">入群</el-link>
       <el-divider direction="vertical"></el-divider>
-      <el-link href="https://bit.ly/2X0NW4e" icon="el-icon-link">每日题列表</el-link>
+      <el-link href="https://bit.ly/2X0NW4e" target="_blank" icon="el-icon-link">每日题列表</el-link>
       <el-divider direction="vertical"></el-divider>
       <el-link :underline="false" @click="otherInfoVisible = true">其他链接</el-link>
       <el-divider direction="vertical"></el-divider>
@@ -19,23 +19,34 @@
 
     <el-dialog title="入群割韭菜" :visible.sync="joiningInfoVisible" center>
       <p>
-        If you are interested in joining this group, please contact: <a href="mailto:guan.huifeng@gmail.com">guan.huifeng@gmail.com</a>.
+        If you are interested in joining this group, please contact:
+        <el-link type="primary" icon="el-icon-message" href="mailto:guan.huifeng@gmail.com">guan.huifeng@gmail.com</el-link>.
       </p>
       <p>
-        Make sure you agree with our <el-link type="primary" href="https://wisdompeak.github.io/lc-score-board/rules.html">terms and regulations</el-link>.
+        Make sure you agree with our <el-link type="primary" href="https://wisdompeak.github.io/lc-score-board/rules.html" target="_blank">terms and regulations</el-link>.
         Think twice before you apply.
       </p>
       <p>
-        [<el-link type="danger" href="https://wisdompeak.github.io/lc-score-board/Blacklist/Darren_Zang.html">Blacklisted Persons</el-link>]
+        [<el-link type="danger" href="https://wisdompeak.github.io/lc-score-board/Blacklist/Darren_Zang.html" target="_blank">Blacklisted Persons</el-link>]
       </p>
     </el-dialog>
 
     <el-dialog title="其他链接" width="40%" :visible.sync="otherInfoVisible" center>
-      <p><el-link type="primary" href="https://docs.google.com/presentation/d/11Vv8RnbGZm8hQEa9T6DhxtMtsBknI1Io0nfcFVVOYUw/">Happy New Year! See 2020 Year-end Review!</el-link></p>
-      <p><el-link type="primary" href="https://wisdompeak.github.io/lc-score-board/cup.html">Looking for more challenges? Try Wisdom Cup!</el-link></p>
-      <p><el-link type="primary" href="https://www.google.com/maps/d/viewer?mid=1c1t3qKsKxTTnDD_P2BsFsrL7p8l9Bou8">See where we are from</el-link></p>
-      <p><el-link type="primary" href="https://wisdompeak.github.io/lc-score-board/sd.html">Cruel System Design Group</el-link></p>
-      <p><el-link type="primary" href="https://wisdompeak.github.io/lc-score-board/graduates.html">Graduated members and archived scores</el-link></p>
+      <p><el-link type="primary" href="https://docs.google.com/presentation/d/11Vv8RnbGZm8hQEa9T6DhxtMtsBknI1Io0nfcFVVOYUw/" target="_blank">
+        Happy New Year! See 2020 Year-end Review!
+      </el-link></p>
+      <p><el-link type="primary" href="https://wisdompeak.github.io/lc-score-board/cup.html" target="_blank">
+        Looking for more challenges? Try Wisdom Cup!
+      </el-link></p>
+      <p><el-link type="primary" href="https://www.google.com/maps/d/viewer?mid=1c1t3qKsKxTTnDD_P2BsFsrL7p8l9Bou8" target="_blank">
+        See where we are from
+      </el-link></p>
+      <p><el-link type="primary" href="https://wisdompeak.github.io/lc-score-board/sd.html" target="_blank">
+        Cruel System Design Group
+      </el-link></p>
+      <p><el-link type="primary" href="https://wisdompeak.github.io/lc-score-board/graduates.html" target="_blank">
+        Graduated members and archived scores
+      </el-link></p>
     </el-dialog>
 
     <el-dialog title="统计（点击看大图）" :visible.sync="curveFormVisible" center>
