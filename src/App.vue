fd3a6e69dfff5e3d33bbc365e65f9038a0aebb3b
Zihong Hao <haozihong@hotmail.com>
2021-09-14 16:57:59 -0400

    update fonts

--- src/App.vue
+++ src/App.vue
@@ -31,7 +31,7 @@ html, body {
 }
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
   height: 100%;
 }
 
