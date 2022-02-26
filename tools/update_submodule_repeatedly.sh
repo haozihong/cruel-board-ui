9394e646a8292d022285161500e12b27ffe9b169
Zihong Hao <haozihong@hotmail.com>
2022-02-26 17:08:22 -0500

    improve script of repeat checking

--- tools/update_submodule_repeatedly.sh
+++ tools/update_submodule_repeatedly.sh
@@ -1,6 +1,16 @@
+# updating gap in second
+time_gap=6
+# total running time: 6 hours (GitHub Action limitation)
+time_last=`expr 359 \* 60`
+time_start=$(date +%s)
+time_cur=$time_start
+
 git pull --recurse-submodules
-while true
+
+for ((i=1; $time_cur - $time_start <= $time_last; i++))
 do
+    echo "[INFO]Trial $i. Running time `expr $time_cur - $time_start`"
     git submodule update --remote --recursive && git commit -am "Auto updated submodule references" && git push
-    sleep 6
+    sleep $time_gap
+    time_cur=$(date +%s)
 done
